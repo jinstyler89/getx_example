46ad944c454c98335a35b15714d222290c6366d5
unknown <jinstyler74@gmail.com>
2022-10-24 00:31:54 +0900

    test

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -15,7 +15,7 @@ class HomeView extends GetView<HomeController> {
       ),
       body: Center(
         child: Text(
-          'HomeView is working',
+          'HomeView',
           style: TextStyle(fontSize: 20),
         ),
       ),
